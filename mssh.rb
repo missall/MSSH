26560bff7ae0b63360bfd71a3de368422128fd6d
Joe Lee <missall2008@gmail.com>
2015-11-17 16:13:15 +0800

    优化mssh脚本文件
    1、增加项目名以及包名获取方式为读取项目目录中的.project
    2、优化ssh框架中的目录结构
    3、文件的增删改增加相应的配色

---
+++ mssh.rb
@@ -0,0 +1,38 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Mssh < Formula
+  homepage "http://missall.github.io/MSSH"
+  url "https://github.com/missall/MSSH/archive/1.1.1.zip"
+  version "1.1.1"
+  sha1 "04623b694ddfdcd6643c7e242ce0687b1bc38380"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test MSSH`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
